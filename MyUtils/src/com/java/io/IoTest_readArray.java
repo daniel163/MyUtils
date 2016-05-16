36909bc833f6db80cf5bf2c2dd3760b56fb8757e
zhanghengwin <zhanghengwin@10.36.44.54>
2016-05-16 15:29:58 +0800

    update

--- MyUtils/src/com/java/io/IoTest_readArray.java
+++ MyUtils/src/com/java/io/IoTest_readArray.java
@@ -3,9 +3,9 @@ package com.java.io;
 
 public class IoTest_readArray {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Integer length = IoUtils.writeObjectArray("F:\\a.txt",new Person[]{
+		Integer length = IoUtils.writeObjectArray("D:\\a.txt",new Person[]{
 				new Person("zhangheng1", "男", 29, "广东深圳", "13670128472"),
 				new Person("zhangheng2", "男", 29, "广东深圳", "13670128472"),
 				new Person("zhangheng3", "男", 29, "广东深圳", "13670128472"),
@@ -13,7 +13,7 @@ public class IoTest_readArray {
 				new Person("zhangheng5", "男", 29, "广东深圳", "13670128472"),
 				new Person("zhangheng6", "男", 29, "广东深圳", "13670128472")}, false);
 		
-		Object[] obj = IoUtils.readArray("F:\\a.txt",length);
+		Object[] obj = IoUtils.readArray("D:\\a.txt",length);
 		for (Object object : obj) {
 			System.out.println(object.toString());
 		}
